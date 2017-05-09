848ae4750e7cfb0235123455d24585588fbc914b
chaoranchen <chen4393@umn.com>
2017-05-08 21:16:12 -0500

    q1

--- a.pl
+++ a.pl
@@ -2,46 +2,57 @@
 # Question: 4a
 use Data::Dumper;
 #Initializing parameters
-$n=100; $m=10; $a = 5; $b = 8;
+$n = 100; $m = 10; 
+$a = 5; $b = 8; # 2 random vertices
 
+# run the simulation for all possible p values
+# p is connected probability
 for($p = 0; $p < 1.01; $p = $p + 0.01) {
 	$sum = 0;
-
-	for(my $iteration=0; $iteration < 100; $iteration++) {
-		%nodeGraph = ();
-		%nodeConnGraph = ();
+	# simulate 100 times
+	for (my $iteration = 0; $iteration < 100; $iteration++) {
+		%nodeGraph = ();# adjacency list
+		%nodeConnGraph = ();# connected adjacency list
 		my $connected = 0;
 		
-		for(my $i = 0; $i < $n; $i = $i + 1) {
+		# build adjacency list
+		for (my $i = 0; $i < $n; $i = $i + 1) {
 			my $currentNode = $i;
 			my @nodeNeighbors = ();
+			# choose m neighbors
 			my $counter = $m;
-			while($counter > 0) {
+			while ($counter > 0) {
 				my $nearby = int(rand($n));
-				# keep generating different random node
-				while(($nearby == $currentNode) || ($nearby ~~ @nodeNeighbors)) {
+				# keep generating different random vertex
+				while (($nearby == $currentNode) || 
+				($nearby ~~ @nodeNeighbors)) {
 					$nearby = int(rand($n));
 				}
 				push @nodeNeighbors, $nearby;
 				$counter = $counter - 1;
 			}
+			# key is vertex index, value is its neighbors
 			$nodeGraph{$i} = \@nodeNeighbors;
 		}
 
-		if($p eq 0) {
+		# build connected adjacency list
+		if ($p eq 0) {
+			# no connected, build an empty list for each vertex
 			my @connectedNeighbors = ();
-			for(my $temp = 0; $temp < 100; $temp++) {
+			for (my $temp = 0; $temp < $n; $temp++) {
 				$nodeConnGraph{$i} = \@connectedNeighbors;
 			}
-		} elsif($p eq 1) {
+		} elsif ($p eq 1) {
+			# all connected, use adjacency list
 			%nodeConnGraph = %nodeGraph;
 		} else {
-			for(my $temp=0; $temp<100; $temp++) {
+			for (my $temp = 0; $temp < $n; $temp++) {
 				my $key = $temp;
 				my @value = @{$nodeGraph{$key}};
 				my @connectedNeighbors = ();
 				while($nearby = shift @value) {
 					my $randomNumber = rand();
+					# regard its neighbor connected with a probability
 					if($randomNumber < $p) {
 						push @connectedNeighbors, $nearby;
 					}
@@ -50,18 +61,27 @@ for($p = 0; $p < 1.01; $p = $p + 0.01) {
 			}
 		}
 
+		# check the connectivity of a random vertex a
 		@nodeQueue = @{$nodeConnGraph{$a}};
-		$queueSize = @nodeQueue;@visitedNodes = ();
+		$queueSize = @nodeQueue;
+		@visitedNodes = ();
 		while($queueSize > 0) {
+			# dump one of a's neighbor as element
 			my $element = shift @nodeQueue;
+			# mark it as visited
 			push @visitedNodes, $element;
-			if($element eq $b) {
+			if ($element eq $b) {
+				# if b is a's neighbor, count it and break
 				$connected = 1;
 				last;
 			} else {
-				my @elementConnections =
-				@{$nodeConnGraph{$element}};
-				while($tempNode = shift @elementConnections) {
+				my @elementConnections = @{$nodeConnGraph{$element}};
+				# scan the neighbors of one of this a's neighbor
+				while ($tempNode = shift @elementConnections) {
+=begin 
+					if this element's neighbor not visited, 
+					push into the queue and prepare for visiting
+=cut 
 					if($tempNode ~~ @visitedNodes) {}
 					else {
 						push @nodeQueue, $tempNode;
@@ -70,10 +90,10 @@ for($p = 0; $p < 1.01; $p = $p + 0.01) {
 			}
 			$queueSize = @nodeQueue;
 		}
-
+		# total times when a and b are connected
 		$sum = $sum + $connected;
 	}
-
-	$probability = $sum/100;
-	print "$p,$probability\n";
+	# after 100 simulations, calculate the connection probability
+	$probability = $sum / 100;
+	print "$p, $probability\n";
 }
--- test.pl
+++ test.pl
@@ -1,5 +1,6 @@
-#!/usr/bin/perl -l
+#!/usr/bin/perl
 
+=begin
 my %color_of = (
     apple  => "red",
     orange => "orange",
@@ -16,16 +17,43 @@ for my $fruit (keys %color_of) {
 }
 
 print scalar keys %color_of;
+=cut
 
-@array = (1..5);
-
-$aref = \@array;
-print aref;
+@array1 = (1..5);
+@array2 = (6..8);
 
 =begin
 while ($element = shift(@array)) {
    print("$element - ");
 }
 print("The End");
-=end
+=cut
+
+%table = ();
+$table{0} = \@array1;
+$table{1} = \@array2;
+$table{3} = \@array1;
+#$aref = \@array;
+
+=begin
+foreach (sort keys %table) {
+	print "$_ : $table{$_}\n";
+}
+=cut
+
+@queue = @{$table{1}};
+#print_array(@queue);
+
+$q_size = @queue;
+
+print "$q_size";
+
+sub print_array {
+	my @list = @_;
+	while ($element = shift(@list)) {
+		print("$element - ");
+	}
+	print("The End");
+}
+
 
